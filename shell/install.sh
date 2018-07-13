a28fed01e82c5d5aed1eb8f7c1ff10bba582e10a
アン <chinaume@163.com>
2018-07-13 12:27:22 +0800

    Create install.sh

---
+++ shell/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo 
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+
+yum install jenkins
